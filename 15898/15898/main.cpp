8858e926724001cf263fc899a0e9a6e73f6d3c5a
ChoiYRim <zerorimchoi@gmail.com>
2021-07-21 19:45:26 +0900

    7.21일 일일 알고리즘

--- 15898/15898/main.cpp
+++ 15898/15898/main.cpp
@@ -13,7 +13,7 @@ struct Kiln
 
 int N,result = 0;
 Kiln kiln[5][5] = {0,};
-Kiln ingredient[10][4][4] = {0,};
+Kiln ingredient[10][2][4][4][4] = {0,}; // [재료 인덱스][(시계 / 반시계)][ 회전 횟수][Y][X]
 vector<pair<int,int>> leftUps;
 vector<vector<int>> allCase;
 
@@ -29,32 +29,27 @@ void init(void)
     }
 }
 
-void turn(int idx,bool clockWise,Kiln arr[4][4])
+void makeTurnedIngredient(int idx,bool clockWise)
 {
-    for(int i = 0; i < 4; i++)
+    for(int turn = 1; turn < 4; turn++)
     {
-        for(int j = 0; j < 4; j++)
+        for(int i = 0; i < 4; i++)
         {
-            if(!clockWise) // counter clock wise
+            for(int j = 0; j < 4; j++)
             {
-                ingredient[idx][i][j].quality = arr[j][3-i].quality;
-                ingredient[idx][i][j].color = arr[j][3-i].color;
-            }
-            else
-            {
-                ingredient[idx][i][j].quality = arr[3-j][i].quality;
-                ingredient[idx][i][j].color = arr[3-j][i].color;
+                if(!clockWise) // counter clock wise
+                {
+                    ingredient[idx][1][turn][i][j].quality = ingredient[idx][1][turn-1][j][3-i].quality;
+                    ingredient[idx][1][turn][i][j].color = ingredient[idx][1][turn-1][j][3-i].color;
+                }
+                else
+                {
+                    ingredient[idx][0][turn][i][j].quality = ingredient[idx][0][turn-1][3-j][i].quality;
+                    ingredient[idx][0][turn][i][j].color = ingredient[idx][0][turn-1][3-j][i].color;
+                }
             }
         }
     }
-}
-
-void turnIngredient(int idx,bool clockWise)
-{
-    Kiln arr[4][4] = {0,};
-    memcpy((Kiln *)arr, (Kiln *)ingredient[idx], sizeof(ingredient[idx]));
-    turn(idx,clockWise,arr);
-    
 }
 
 int getQuality(void)
@@ -80,21 +75,23 @@ int getQuality(void)
     return (7*r+3*g+5*b+2*y);
 }
 
-void putIngredient(int idx,pair<int,int> leftUp)
+void putIngredient(int idx,pair<int,int> leftUp,int turn)
 {
     int y = leftUp.first,x = leftUp.second;
     int inY = 0,inX = 0;
+    int clockWise,turnIdx;
     
     if((y > 1 || x > 1) || (y < 0 || x < 0))
         return;
     
+    clockWise = turn/4; turnIdx = turn%4;
     for(int i = y; i < 5 && inY < 4; i++)
     {
         for(int j = x; j < 5 && inX < 4; j++)
         {
-            if(ingredient[idx][inY][inX].color != 'W')
-                kiln[i][j].color = ingredient[idx][inY][inX].color;
-            kiln[i][j].quality += ingredient[idx][inY][inX].quality;
+            if(ingredient[idx][clockWise][turnIdx][inY][inX].color != 'W')
+                kiln[i][j].color = ingredient[idx][clockWise][turnIdx][inY][inX].color;
+            kiln[i][j].quality += ingredient[idx][clockWise][turnIdx][inY][inX].quality;
             if(kiln[i][j].quality < 0)
                 kiln[i][j].quality = 0;
             else if(kiln[i][j].quality > 9)
@@ -108,13 +105,13 @@ void putIngredient(int idx,pair<int,int> leftUp)
 
 inline int MAX(int x,int y) { return (x > y ? x : y); }
 
-void DFS(int idx,vector<int> comb,vector<pair<int,int>> coordinate)
+void DFS(int idx,vector<int> comb,vector<pair<int,int>> coordinate,vector<int> turn)
 {
     if(idx >= 3)
     {
         init();
         for(int i = 0; i < (int)coordinate.size(); i++)
-            putIngredient(comb[i],coordinate[i]);
+            putIngredient(comb[i],coordinate[i],turn[i]);
         result = MAX(result,getQuality());
         return;
     }
@@ -122,21 +119,14 @@ void DFS(int idx,vector<int> comb,vector<pair<int,int>> coordinate)
     for(int i = 0; i < 4; i++)
     {
         coordinate.push_back(leftUps[i]);
-        Kiln save[4][4] = {0,};
-        memcpy((Kiln *)save, (Kiln *)ingredient[comb[idx]], sizeof(ingredient[comb[idx]]));
         
-        for(int j = 0; j < 3; j++)
+        for(int j = 0; j < 6; j++)
         {
-            turnIngredient(comb[idx],false);
-            DFS(idx+1,comb,coordinate);
+            turn.push_back(j);
+            DFS(idx+1,comb,coordinate,turn);
+            turn.pop_back();
         }
-        memcpy((Kiln *)ingredient[comb[idx]], (Kiln *)save, sizeof(ingredient[comb[idx]]));
-        for(int j = 0; j < 3; j++)
-        {
-            turnIngredient(comb[idx],true);
-            DFS(idx+1,comb,coordinate);
-        }
-        memcpy((Kiln *)ingredient[comb[idx]], (Kiln *)save, sizeof(ingredient[comb[idx]]));
+        
         coordinate.pop_back();
     }
 }
@@ -189,23 +179,28 @@ int main(int argc,char *argv[])
         {
             int temp;
             cin >> temp;
-            ingredient[i][j/4][j%4].quality = temp;
+            ingredient[i][0][0][j/4][j%4].quality = temp;
+            ingredient[i][1][0][j/4][j%4].quality = temp;
         }
         for(int j = 0; j < 16; j++)
         {
             char temp;
             cin >> temp;
-            ingredient[i][j/4][j%4].color = temp;
+            ingredient[i][0][0][j/4][j%4].color = temp;
+            ingredient[i][1][0][j/4][j%4].color = temp;
         }
     }
     for(int i = 0; i < N; i++)
+    {
         perm.push_back(i);
-    
+        makeTurnedIngredient(i,true);
+        makeTurnedIngredient(i,false);
+    }
     leftUps = {{0,0},{0,1},{1,0},{1,1}};
     permutation(perm,0,N,3);
     //combination(comb, 3, 0, 0);
     for(int i = 0; i < (int)allCase.size(); i++)
-        DFS(0,allCase[i],{});
+        DFS(0,allCase[i],{},{});
     cout << result << '\n';
     return 0;
 }
